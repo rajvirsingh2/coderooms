2007e5cf7a131d43de3f8eb85dd9b22629678d75
rajvirsingh2 <rajvirsingh1009f@gmai.com>
2025-05-21 04:08:01 +0530

    Resources Shrinked

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,13 +15,13 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
